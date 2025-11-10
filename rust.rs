a6dc1a556de342796a081f017cd3e68f4c3abe9c
Yellowsam <69874104+Yellowsam4145@users.noreply.github.com>
2025-11-10 14:59:39 -0800

    The start of a new rust file

---
+++ rust.rs
@@ -0,0 +1,14 @@
+// A new snake project begins...
+println!("Loading...");
+
+// Screen fuctions
+fn showcreen() {
+    // code to show an 8 by 8 screen
+    
+}
+
+fn setpoint(x, y, icon: usize) {
+    // code to set a point on the screen
+    
+
+}
